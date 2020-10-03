54bd602dd7963789ab038f217a54f2e6b9bb4a6c
Andrzej <andrzej@devroom.pl>
2020-10-03 10:08:56 +0200

    ball

--- src/ball.rs
+++ src/ball.rs
@@ -1,21 +1,44 @@
-use crate::{WINDOW_WIDTH, WINDOW_HEIGHT};
-use ggez::graphics::{Mesh};
-use ggez::{Context, GameResult, GameError, graphics};
+use crate::{BALL_SIZE, WINDOW_HEIGHT, WINDOW_WIDTH};
+use ggez::graphics::Mesh;
 use ggez::nalgebra::{Point2, Vector2};
-pub struct Ball{
+use ggez::{graphics, Context, GameError, GameResult};
+pub struct Ball {
     pub loc: Point2<f32>,
     pub vel: Vector2<f32>,
 }
 
 impl Ball {
-    pub fn new () -> Ball{
+    pub fn new() -> Ball {
         Ball {
-            loc: Point2::new(WINDOW_WIDTH/2.0,WINDOW_HEIGHT/2.0),
-            vel: Vector2::new(0.0,0.0)
+            loc: Point2::new(WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0),
+            vel: Vector2::new(5.0, 5.0),
         }
     }
-    pub fn draw (&self, ctx: &mut Context) -> GameResult<()> {
-        let circle = Mesh::new_circle(ctx,graphics::DrawMode::fill(), self.loc, 10.0, 2.0, graphics::BLACK)?;
-        graphics::draw(ctx, &circle, (self.loc,))
+    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        let circle = Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            Point2::origin(),
+            BALL_SIZE,
+            1.0,
+            graphics::BLACK,
+        )?;
+        graphics::draw(ctx, &circle, (self.loc,))?;
+        Ok(())
+    }
+    pub fn update(&mut self) -> GameResult<()> {
+        self.loc = self.loc + self.vel;
+        self.collides();
+        Ok(())
+    }
+    pub fn collides(&mut self) {
+        // checl wall collision
+        if self.loc.y >= WINDOW_HEIGHT - BALL_SIZE {
+            self.vel.y = self.vel.y * -1.0;
+            self.loc.y = WINDOW_HEIGHT - BALL_SIZE;
+        } else if self.loc.y <= 0.0 {
+            self.vel.y = self.vel.y * -1.0;
+            self.loc.y = 0.0;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use ggez::{graphics, Context, ContextBuilder, GameResult};
 use ggez::event::{self, EventHandler};
+use ggez::{graphics, Context, ContextBuilder, GameResult};
 
 mod ball;
 
-use crate::ball::{Ball};
+use crate::ball::Ball;
 
 pub const WINDOW_WIDTH: f32 = 800.0;
 pub const WINDOW_HEIGHT: f32 = 600.0;
+pub const BALL_SIZE: f32 = 10.0;
 
 fn main() {
     // Make a Context.
     let (mut ctx, mut event_loop) = ContextBuilder::new("my_game", "Cool Game Author")
-		.build()
-		.expect("aieee, could not create ggez context!");
+        .window_mode(ggez::conf::WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT))
+        .build()
+        .expect("aieee, could not create ggez context!");
 
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object to
@@ -22,34 +24,34 @@ fn main() {
     // Run!
     match event::run(&mut ctx, &mut event_loop, &mut my_game) {
         Ok(_) => println!("Exited cleanly."),
-        Err(e) => println!("Error occured: {}", e)
+        Err(e) => println!("Error occured: {}", e),
     }
 }
 
 struct Pong {
     // Your state here...
+    ball: Ball,
 }
 
 impl Pong {
     pub fn new(_ctx: &mut Context) -> Pong {
         // Load/create resources such as images here.
-        Pong {
-            // ...
-        }
+        let ball = Ball::new();
+        Pong { ball }
     }
 }
 
 impl EventHandler for Pong {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         // Update code here...
+        self.ball.update()?;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, graphics::WHITE);
         // Draw code here...
-        let ball = Ball::new();
-        ball.draw(ctx)?;
+        self.ball.draw(ctx)?;
         graphics::present(ctx)
     }
 }
